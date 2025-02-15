ee2807140d7e0e92f80ab6bcd320828d2df394d9
Slava Morozov <155018849+slavamorozov23@users.noreply.github.com>
2025-02-15 22:08:33 +0500

    Update InspectorReadOnlyDrawer.cs

--- Scripts/InspectorReadOnlyDrawer.cs
+++ Scripts/InspectorReadOnlyDrawer.cs
@@ -9,8 +9,9 @@ public class InspectorReadOnlyAttribute : PropertyAttribute { }
 [CustomEditor(typeof(MonoBehaviour), true)]
 public class DefaultReadOnlyEditor : Editor
 {
-    static Dictionary<Object, Dictionary<MemberInfo, (object value, float time, bool changed)>> cache
+    static Dictionary<Object, Dictionary<MemberInfo, (object value, float changeTime, bool changed)>> cache
         = new Dictionary<Object, Dictionary<MemberInfo, (object, float, bool)>>();
+    static Dictionary<System.Type, MemberInfo[]> memberCache = new Dictionary<System.Type, MemberInfo[]>();
 
     public override void OnInspectorGUI()
     {
@@ -20,31 +21,48 @@ public class DefaultReadOnlyEditor : Editor
             if (!cache.ContainsKey(o))
                 cache[o] = new Dictionary<MemberInfo, (object, float, bool)>();
 
-            var members = o.GetType().GetMembers(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+            var type = o.GetType();
+            if (!memberCache.TryGetValue(type, out var members))
+            {
+                members = type.GetMembers(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+                memberCache[type] = members;
+            }
+
             foreach (var m in members)
             {
                 if (m.GetCustomAttribute<InspectorReadOnlyAttribute>() == null) continue;
                 EditorGUI.BeginDisabledGroup(true);
 
-                object val = null;
-                if (m is FieldInfo f) val = f.GetValue(o);
-                else if (m is PropertyInfo p) val = p.GetValue(o);
-
-                if (!cache[o].TryGetValue(m, out var old))
-                    cache[o][m] = (val, (float)EditorApplication.timeSinceStartup, false);
-
-                var now = (float)EditorApplication.timeSinceStartup;
-                bool changed = !AreEqual(old.value, val);
-                cache[o][m] = (val, now, changed);
-
-                EditorGUILayout.LabelField(m.Name, val == null ? "null" : val.ToString());
-
-                if (now - old.time < 2)
+                object val = m is FieldInfo f ? f.GetValue(o) : (m is PropertyInfo p ? p.GetValue(o) : null);
+                float now = (float)EditorApplication.timeSinceStartup;
+                if (!cache[o].TryGetValue(m, out var record))
                 {
-                    var style = new GUIStyle(EditorStyles.label) { normal = { textColor = Color.green } };
-                    EditorGUILayout.LabelField(changed ? "Value updated" : "Value updated (but not changed)", style);
+                    record = (val, now, false);
+                    cache[o][m] = record;
+                }
+                else
+                {
+                    if (!AreEqual(record.value, val))
+                    {
+                        if (!record.changed)
+                            record = (val, now, true);
+                        else
+                            record = (val, record.changeTime, true);
+                        cache[o][m] = record;
+                    }
+                    else if (record.changed && now - record.changeTime >= 2f)
+                    {
+                        record = (val, record.changeTime, false);
+                        cache[o][m] = record;
+                    }
                 }
 
+                EditorGUILayout.LabelField(m.Name, val == null ? "null" : val.ToString());
+                if (cache[o][m].changed && now - cache[o][m].changeTime < 2f)
+                {
+                    var style = new GUIStyle(EditorStyles.label) { normal = { textColor = Color.green } };
+                    EditorGUILayout.LabelField("Value updated", style);
+                }
                 EditorGUI.EndDisabledGroup();
             }
         }
